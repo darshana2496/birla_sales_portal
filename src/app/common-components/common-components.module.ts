f73830e66ef661cde07fb6862c232ed6dc525927
darshana2496 <darshana@digitalsalt.in>
2023-01-07 14:14:19 +0530

    header component creation

---
+++ src/app/common-components/common-components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
+@NgModule({
+    declarations: [
+        HeaderComponent
+    ],
+    imports: [IonicModule],
+    providers: [],
+    bootstrap: [],
+  })
+  export class CommonComponentsModule {}
