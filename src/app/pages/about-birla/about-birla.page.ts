f06985fcd5d993cd337631b26adeebe8f87f5776
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-01-09 11:10:30 +0530

    url changes image and scss

---
+++ src/app/pages/about-birla/about-birla.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-birla',
+  templateUrl: './about-birla.page.html',
+  styleUrls: ['./about-birla.page.scss'],
+})
+export class AboutBirlaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
