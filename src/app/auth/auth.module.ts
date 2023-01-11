ee62064c30fa94df356aeb6e809a1f75ff0dee40
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-01-11 19:42:38 +0530

    set-pin done

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonComponentsModule } from './../common-components/common-components.module';
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -11,6 +11,8 @@ import { LoginComponent } from './login/login.component';
 import { AppIntroPage } from './app-intro/app-intro.component';
 import { KnowYourCustIdPage } from './know-your-cust-id/know-your-cust-id.component';
 import { LandingPage } from './landing/landing.component';
+import { SetPinComponent } from './set-pin/set-pin.component';
+import { OnlyNumberDirective } from '../utilities/directives/only-number.directive';
 
 
 @NgModule({
@@ -20,13 +22,15 @@ import { LandingPage } from './landing/landing.component';
     CommonModule,
     FormsModule,
     AuthRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AuthComponent, 
     LoginComponent,
     AppIntroPage,
     KnowYourCustIdPage,
-    LandingPage
+    LandingPage,
+    SetPinComponent,OnlyNumberDirective
   ],
 })
 export class AuthModule { }
